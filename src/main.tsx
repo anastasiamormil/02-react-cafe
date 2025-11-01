77325acb38eed266107fbb536316a05a2d7c6f55
Anastasia <mormilanastasia5@gmail.com>
2025-11-01 11:05:32 +0100

    Add modern-normalize package and add styles.css for main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import App from "./components/App/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "modern-normalize";
+import "style.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
